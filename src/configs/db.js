15b75dc1b975fd87cd77ea43d41df568ca093db6
shayan <shayanjafarnejad75@gmail.com>
2025-09-27 10:09:41 +0330

    connect to the database

---
+++ src/configs/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectToDb = async () => {
+  if (mongoose.connections[0].readyState) return;
+
+  try {
+    await mongoose.connect("mongodb://localhost:27017/todoList", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to DB");
+  } catch (error) {
+    console.error("❌ DB connection error:", error);
+  }
+};
+
+export default connectToDb;
