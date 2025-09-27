fc90515bc5d1499f75486ec2f4d2b28898f7f8d7
shayan <shayanjafarnejad75@gmail.com>
2025-09-27 10:40:30 +0330

    set style for navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import style from '../styles/todolist.css'
-import { CiCirclePlus } from "react-icons/ci";
+import { FaPlus } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <div className='navbar'>
-      <h1 className='title-project'>Todo List</h1>
-      <h3 className='btn-add-new-todo'>Add New Todo <CiCirclePlus /></h3>
+        <h1 className='title-project'>Todo List</h1>
+        <h3 className='btn-add-new-todo'>Add New Todo <FaPlus className='icon-add-todo' /></h3>
       </div>
-    </div>
+      <div className="container">
+      </div>
+    </>
   );
 }
