edeed41867e897185d2278baf516ad4a8f50c77f
shayan <shayanjafarnejad75@gmail.com>
2025-09-27 10:27:26 +0330

    develop navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import style from '../styles/todolist.css'
+import { CiCirclePlus } from "react-icons/ci";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className="container">
+      <div className='navbar'>
+      <h1 className='title-project'>Todo List</h1>
+      <h3 className='btn-add-new-todo'>Add New Todo <CiCirclePlus /></h3>
+      </div>
     </div>
   );
 }
