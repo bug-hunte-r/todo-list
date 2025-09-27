b7b4727cab059b8c5b8b69f47470b2575113c147
shayan <shayanjafarnejad75@gmail.com>
2025-09-27 14:05:56 +0330

    develop modal for add new todo

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,37 @@ import style from '../styles/todolist.css'
 import media from '../styles/media-TodoList.css'
 import { FaPlus } from "react-icons/fa";
 import TodoCard from '@/components/modules/Todo-card';
+import { IoMdClose } from "react-icons/io";
 
 export default function Home() {
   return (
     <>
+
       <div className='navbar'>
         <h1 className='title-project'>Todo List</h1>
         <h3 className='btn-add-new-todo'>Add New Todo <FaPlus className='icon-add-todo' /></h3>
       </div>
+
       <div className="container">
         <TodoCard />
         <TodoCard />
         <TodoCard />
         <TodoCard />
       </div>
+
+      <div className='container-modal'>
+        <div className='close-and-title'>
+          < IoMdClose />
+          <h2>Add Todo</h2>
+        </div>
+        <input type='text' placeholder='Title' className='inputs-add-todo' />
+        <input type='text' placeholder='Description' className='inputs-add-todo' />
+        <h3 className='title-Priority'>Select the priority level for this todo.</h3>
+        <button className='btn-Priority-levels'>Low</button>
+        <button className='btn-Priority-levels'>Medium</button>
+        <button className='btn-Priority-levels'>High</button>
+      </div>
+
     </>
   );
 }
