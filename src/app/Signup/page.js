44730907f5d7a9e681ef72a0b1268c0fc38c7a1c
shayan <shayanjafarnejad75@gmail.com>
2025-09-28 22:45:06 +0330

    set style for signup form

--- src/app/Signup/page.js
+++ src/app/Signup/page.js
@@ -2,19 +2,23 @@ import React from 'react'
 import style from '../../styles/Signup-form/SignupForm.css'
 import media from '../../styles/Signup-form/mediaSignupForm.css'
 import { FaArrowLeft } from "react-icons/fa6";
+import Link from 'next/link';
 
 function Signup() {
-  return (
-    <div className='form-container'>
-        <FaArrowLeft className='icon-form' />
-        <div className='form'>
-            <h2 className='title-form'>Signup</h2>
-            <input type='text' placeholder='Username' className='inputs-form' />
-            <input type='password' placeholder='Password' className='inputs-form' />
-            <button className='btn-form'>Signup</button>
+    return (
+        <div className='form-container'>
+            <div className='form'>
+                <div className='icon-and-title'>
+                    <FaArrowLeft className='icon-form' />
+                    <h2 className='title-form'>Signup</h2>
+                </div>
+                    <input type='text' placeholder='Username' className='inputs-form' />
+                    <input type='password' placeholder='Password' className='inputs-form' />
+                    <p className='text-form'>Already have an account? <Link href={'/Login'} className='link-form'>Login</Link></p>
+                    <button className='btn-form'>Signup</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Signup
