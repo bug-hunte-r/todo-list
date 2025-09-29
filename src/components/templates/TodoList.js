91e29e94bb0ea1e57a9702f12da8bf186ffa9fb9
shayan <shayanjafarnejad75@gmail.com>
2025-09-29 10:37:47 +0330

    feth the logout api

--- src/components/templates/TodoList.js
+++ src/components/templates/TodoList.js
@@ -62,7 +62,14 @@ function TodoList() {
         }
         getUsersInfo()
     }, [])
-    
+
+    const logoutHandler = async () => {
+        const res = await fetch('http://localhost:3000/api/auth/logout')
+        const data = await res.json()
+
+        alert(data.message)
+    }
+
     return (
         <>
             <div className='navbar'>
