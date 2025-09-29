9502ae4ab49b47b141a53befb8c4b964bc1d4bb8
shayan <shayanjafarnejad75@gmail.com>
2025-09-29 08:55:22 +0330

    protect the signup route

--- src/app/Signup/page.js
+++ src/app/Signup/page.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import SignupForm from '@/components/templates/Signup-form';
+import { getUser } from '@/configs/authHelper';
+import { redirect } from 'next/navigation';
 
-function Signup() {
+async function Signup () {
+
+    const isUserLogin = await getUser()
+
+    if (isUserLogin) {
+        redirect('/')
+    }
+
     return (
         <>
             <SignupForm />
--- src/components/templates/Login-form.js
+++ src/components/templates/Login-form.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '../../styles/Signup-form/SignupForm.css'
 import media from '../../styles/Signup-form/mediaSignupForm.css'
 import { FaArrowLeft } from "react-icons/fa6";
