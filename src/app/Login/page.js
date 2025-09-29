559e4622b88b21d1d4193283e5d4ed109e4f1c07
shayan <shayanjafarnejad75@gmail.com>
2025-09-29 08:56:18 +0330

    protect the login route

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,8 +1,17 @@
 import LoginForm from '@/components/templates/Login-form'
+import { getUser } from '@/configs/authHelper'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 
-function Login() {
+async function Login() {
+
+    const isUserLogin = await getUser()
+
+    if (isUserLogin) {
+        redirect('/')
+    }
+
     return (
         <>
             <LoginForm />
